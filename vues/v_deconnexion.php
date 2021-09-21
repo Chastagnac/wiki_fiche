082dad8c9cdd53913cc517ed99c035e872af7027
leochastagnac <leochastagnac@gmail.com>
2021-09-21 17:27:58 +0200

    Changement demonstration commit

--- vues/v_deconnexion.php
+++ vues/v_deconnexion.php
@@ -13,8 +13,10 @@ deconnecter();
 ?>
 
 <div class="alert alert-info" role="alert">
-    <p>Vous avez bien été déconnecté ! <a href="index.php">Cliquez ici</a>
+    <p>Vous avez bien été déconnecté. <a href="index.php">Cliquez ici</a>
         pour revenir à la page de connexion.</p>
 </div>
 <?php
 header("Refresh: 3;URL=index.php");
+
+
