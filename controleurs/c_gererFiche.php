c3c515d91950a3a331ed3fe573e4c6dd28d55c3d
leochastagnac <leochastagnac@gmail.com>
2021-09-25 14:35:25 +0200

    SCRUM008 PART 1

--- controleurs/c_gererFiche.php
+++ controleurs/c_gererFiche.php
@@ -21,12 +21,20 @@ switch ($action) {
         break;
 
     case 'insererFiche':
+
+        include('vues/v_creerFiche.php');
+        break;
+    case 'insert':
         $idCategorie = filter_input(INPUT_POST, 'categorie', FILTER_SANITIZE_NUMBER_INT);
         $libelle = filter_input(INPUT_POST, 'libelle', FILTER_SANITIZE_STRING);
         $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
         $contenu = filter_input(INPUT_POST, 'contenu', FILTER_SANITIZE_STRING);
-        $fiches = $pdo->insertFiches($idCategorie, $idCompte, $libelle, $description, $contenu);
-        include('vues/v_creerFiche.php');
-        break;
-    
+        checkFiche($libelle, $description, $contenu);
+        if (nbErreurs() !== 0) {
+            include 'vues/v_erreurs.php';
+            include 'vues/v_creerFiche.php';
+        } else {
+            $fiches = $pdo->insertFiches($idCategorie, $idCompte, $libelle, $description, $contenu);
+            include 'vues/v_successful.php';
+        }
 }
